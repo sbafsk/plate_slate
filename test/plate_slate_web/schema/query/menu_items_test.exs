b12bbc02dcf7fa3a22f9b1f82197a4392d01c8cd
sbafsk <razor1836@gmail.com>
2024-01-23 14:46:46 -0300

    add items test

---
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -0,0 +1,99 @@
+defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
+  use PlateSlateWeb.ConnCase, async: true
+
+  setup do
+    PlateSlate.Seeds.run()
+  end
+
+  @query """
+  {
+    menuItems {
+      name
+    }
+  }
+  """
+
+  test "menuItems field returns menu items" do
+    conn = build_conn()
+    conn = get conn, "/api", query: @query
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"},
+                 %{"name" => "Croque Monsieur"},
+                 %{"name" => "Muffuletta"},
+                 %{"name" => "Bánh mì"},
+                 %{"name" => "Vada Pav"},
+                 %{"name" => "French Fries"},
+                 %{"name" => "Papadum"},
+                 %{"name" => "Pasta Salad"},
+                 %{"name" => "Water"},
+                 %{"name" => "Soft Drink"},
+                 %{"name" => "Lemonade"},
+                 %{"name" => "Masala Chai"},
+                 %{"name" => "Vanilla Milkshake"},
+                 %{"name" => "Chocolate Milkshake"}
+               ]
+             }
+           }
+  end
+
+  @query """
+  {
+    menuItems(matching: "reu") {
+      name
+    }
+  }
+  """
+
+  test "menuItems field returns menu items filtered by name" do
+    respose = get(build_conn(), "/api", query: @query)
+
+    assert json_response(respose, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"}
+               ]
+             }
+           }
+  end
+
+  @query """
+  {
+    menuItems(matching: 123) {
+      name
+    }
+  }
+  """
+
+  test "menuItems field returns errors when using a bad value" do
+    respose = get(build_conn(), "/api", query: @query)
+
+    assert %{"errors" => [%{"message" => message}]} = json_response(respose, 200)
+
+    assert message == "Argument \"matching\" has invalid value 123."
+  end
+
+  @query """
+  query ($term: String){
+    menuItems(matching: $term) {
+      name
+    }
+  }
+  """
+
+  @variables %{"term" => "reu"}
+
+  test "menuItems field filters by name when using a variable" do
+    respose = get(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert json_response(respose, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"}
+               ]
+             }
+           }
+  end
+end
